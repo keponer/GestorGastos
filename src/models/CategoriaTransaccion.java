dc2122d78c349c5e89081d6c2a805d615286d5b0
Ángel <angel.ayora.93@gmail.com>
2017-05-23 18:33:51 +0200

    Archivos sin guardar

--- src/models/CategoriaTransaccion.java
+++ src/models/CategoriaTransaccion.java
@@ -10,5 +10,25 @@ package models;
  * @author angel
  */
 public class CategoriaTransaccion {
+
+    public float getIva() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    public String getName() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    public void setId(int aInt) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    public void setName(String string) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    public void setIva(float aFloat) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
     
 }
--- src/models/DAO/JDBC/TransaccionDAOJdbc.java
+++ src/models/DAO/JDBC/TransaccionDAOJdbc.java
@@ -21,6 +21,10 @@ public class TransaccionDAOJdbc implements TransaccionDAO{
         this.c = c;
     }
 
+    public TransaccionDAOJdbc(Connection c, Transaccion t) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
     @Override
     public List<Transaccion> getList() {
         throw new UnsupportedOperationException("Not supported yet.");
